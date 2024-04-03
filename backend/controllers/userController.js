43b8ccfcdb2b9a88821e77a112c75e9b1789891c
sujitoic999 <sujitfgiet@gmail.com>
2024-04-03 23:03:01 +0530

    completed till video 44 but admin functionality not working properly

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,39 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 //@desc Auth user & get token
 //@route POST /api/users/login
 //@access Public
 
 const authUser = asyncHandler(async (req, res) => {
-  res.send("auth user");
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userTd: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d", //30 days
+    });
+
+    //Set JWT as HTTP-Only cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000, //30days
+    });
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+
+  // res.send("auth user");
 });
 
 // @desc Register user
@@ -21,7 +48,11 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access Private
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 //@desc Get user profile
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -11,6 +12,14 @@ const port = process.env.PORT || 5000;
 connectDB(); //Connect to MongoDB
 
 const app = express();
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
