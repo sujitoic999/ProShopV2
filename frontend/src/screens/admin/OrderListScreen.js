a745f57fb1ece5db2e57b89ac0078bb38fec5c4b
sujitoic999 <sujitfgiet@gmail.com>
2024-05-03 00:53:41 +0530

    completed 2:13 of video 67

--- frontend/src/screens/admin/OrderListScreen.js
+++ frontend/src/screens/admin/OrderListScreen.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button } from "react-bootstrap";
+import { FaTimes } from "react-icons/fa";
+import Message from "../../components/Loader";
+import { useGetOrdersQuery } from "../../slices/orderApiSlice";
 
 const OrderListScreen = () => {
   return <div className="container">OrderListScreen</div>;
