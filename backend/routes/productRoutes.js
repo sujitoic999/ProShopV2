30569aafc2933a5a2100f427255ac4eadf1558bd
sujitoic999 <sujitfgiet@gmail.com>
2024-05-07 00:26:32 +0530

    4:29 of video 71

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,11 +4,11 @@ import {
   createProduct,
   getProductById,
   getProducts,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById);
-// router.route();
+router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
 
 export default router;
