a5b48dea0aa6832ce828b9a4389959a7f431145a
sujitoic999 <sujitfgiet@gmail.com>
2024-05-19 10:09:37 +0530

    completed till 3:30 mints of video 86

---
+++ frontend/src/components/ProductCarousel.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Carousel, Image } from "react-bootstrap";
+import Loader from "./Loader";
+import Message from "./Message";
+import { useGetTopProductsQuery } from "../slices/productsApiSlice";
+const ProductCarousel = () => {
+  return <div></div>;
+};
+
+export default ProductCarousel;
